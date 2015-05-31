480f0cf7af638c5742d49e2d9466429206c95044
Willem Meints <willem@mein-design.nl>
2015-05-31 13:16:39 +0200

    Add actors project

---
+++ actors/build.sbt
@@ -0,0 +1,5 @@
+name := "ReactivePI-Actors"
+
+version := "0.0.1"
+
+scalaVersion:= "2.11.6"
--- project/build.scala
+++ project/build.scala
@@ -3,5 +3,9 @@ import Keys._
 
 object ReactivePIBuild extends Build {
   lazy val core = Project(id = "core", base = file("core"))
-  lazy val reactivepi = Project(id = "reactivepi", base = file(".")) aggregate("core")
+  lazy val actors = Project(id = "actors",
+    base = file("actors")) dependsOn("core")
+
+  lazy val reactivepi = Project(id = "reactivepi",
+    base = file(".")) aggregate("core","actors")
 }
