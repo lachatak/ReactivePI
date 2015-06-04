70cf50ce2356a9626dfd3b46abf9eb4c02f69a47
Willem Meints <willem@mein-design.nl>
2015-06-04 17:46:40 +0200

    Add subscription logic to GPIO

--- actors/src/main/scala/reactivepi/GPIO.scala
+++ actors/src/main/scala/reactivepi/GPIO.scala
@@ -2,6 +2,9 @@ package reactivepi
 
 import akka.actor.{ActorRef, ActorRefFactory}
 
+/**
+ * Provides general access to the GPIO ports on your raspberry PI
+ */
 object GPIO {
 
   case object Read
@@ -22,7 +25,20 @@ object GPIO {
   }
 
   /**
+   * Subscribes to a specific input pin. When the value changes, the target actor will
+   * receive a GPIO.Data message containing the new value
+   *
+   * @param pinNumber         Pin number to subscribe to
+   * @param actorRefFactory   actorRefFactory to use
+   * @return Returns the created actor
+   */
+  def subscribe(pinNumber: Int, target: ActorRef)(implicit actorRefFactory: ActorRefFactory): ActorRef = {
+    actorRefFactory.actorOf(GPIOSubscriptionActor.props(pinNumber, target))
+  }
+
+  /**
    * Starts an actor for a specific output pin
+   *
    * @param pinNumber        Pin number to use
    * @param actorRefFactory  actorRefFactory to use
    * @return Returns the created actor
---
+++ actors/src/main/scala/reactivepi/GPIOSubscriptionActor.scala
@@ -0,0 +1,47 @@
+package reactivepi
+
+import akka.actor.{Props, ActorRef, Actor}
+import reactivepi.gpio.GPIODriver
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+object GPIOSubscriptionActor {
+  val Tick = "tick"
+
+  def props(pinNumber: Int, target: ActorRef) = Props(new GPIOSubscriptionActor(pinNumber, target))
+}
+
+class GPIOSubscriptionActor(pinNumber: Int, target: ActorRef) extends Actor {
+
+  import GPIOSubscriptionActor._
+
+  val inputPin = GPIODriver.input(pinNumber)
+  var currentValue = inputPin.read()
+
+  // Read the initial value and start the scheduler
+  readInputValue(initial = true)
+  context.system.scheduler.schedule(1 millisecond, 1 millisecond, self, Tick)
+
+  // Every time the timeout on the scheduler is triggered
+  // the input value is read and send to the target actor.
+  def receive = {
+    case Tick => readInputValue(initial = false)
+  }
+  
+  private def readInputValue(initial: Boolean): Unit = {
+    val inputValue = inputPin.read()
+
+    if(inputValue != currentValue || initial) {
+      target ! GPIO.Data(inputValue)
+    }
+
+    currentValue = inputValue
+  }
+
+  @throws[Exception](classOf[Exception])
+  override def postStop(): Unit = {
+    super.postStop()
+    inputPin.close()
+  }
+}
