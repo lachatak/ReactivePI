d638f20f6efdaefabc39c1b043715a822fa233ea
Willem Meints <willem@mein-design.nl>
2015-08-09 17:09:43 +0200

    Fix C driver file

--- core/src/jni/nl_fizzylogic_reativepi_i2c_I2CDevice.c
+++ core/src/jni/nl_fizzylogic_reativepi_i2c_I2CDevice.c
@@ -1,25 +1,38 @@
+#include <jni.h>
 #import "nl_fizzylogic_reactivepi_i2c_I2CDevice.h"
 
-JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_initializeDevice(JNIEnv *, jobject, jstring) {
+JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_initializeDevice
+    (JNIEnv * env, jobject instance, jstring path)
+{
     return -1;
 }
 
-JNIEXPORT void JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_closeDevice(JNIEnv *, jobject, jint) {
+JNIEXPORT void JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_closeDevice
+    (JNIEnv * env, jobject instance, jint handle)
+{
+
+}
+
+JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_deviceWriteDirect
+    (JNIEnv * env, jobject instance, jint handle, jint deviceAddress, jbyteArray data, jint offset, jint length)
+{
     return -1;
 }
 
-JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_deviceWriteDirect(JNIEnv *, jobject, jint, jint, jbyteArray, jint, jint) {
+JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_deviceWrite
+    (JNIEnv *env, jobject instance, jint handle, jint deviceAddress, jint deviceRegister, jbyteArray data, jint offset, jint length)
+{
     return -1;
 }
 
-JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_deviceWrite(JNIEnv *, jobject, jint, jint, jint, jbyteArray, jint, jint) {
+JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_deviceReadDirect
+    (JNIEnv *env, jobject instance, jint handle, jint deviceAddress, jbyteArray data, jint offset, jint length)
+{
     return -1;
 }
 
-JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_deviceReadDirect(JNIEnv *, jobject, jint, jint, jbyteArray, jint, jint) {
-    return -1;
-}
-
-JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_deviceRead(JNIEnv *, jobject, jint, jint, jint, jbyteArray, jint, jint) {
+JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_deviceRead
+    (JNIEnv *env, jobject instance, jint handle, jint deviceAddress, jint deviceRegister, jbyteArray data, jint offset, jint length)
+{
     return -1;
 }
