c1b320e8668b600ebf5d0d40666311f8fc762ada
Willem Meints <willem@mein-design.nl>
2015-08-09 16:44:02 +0200

    Prepare for native component

--- core/build.sbt
+++ core/build.sbt
@@ -3,3 +3,28 @@ name := "core"
 organization := "nl.fizzylogic.reactivepi"
 
 scalaVersion := "2.11.6"
+
+
+
+val nativeClasses = List(
+  "nl.fizzylogic.reactivepi.i2c.I2CDevice"
+)
+
+val nativeDeviceSources = List(
+  "src/jni/nl_fizzylogic_reativepi_i2c_I2CDevice.c"
+)
+
+val nativeGenerateHeaders = taskKey[Int]("Generates JNI headers for the library")
+val nativeCompile = taskKey[Int]("Compiles the native library")
+
+nativeGenerateHeaders := {
+  ("javah -classpath target/scala-2.11/classes -d src/jni " + nativeClasses.mkString(" ")) !
+}
+
+nativeCompile := {
+  val compileCommand = "gcc -Wl -add-stdcall-alias -I\"$JAVA_HOME/include/\" -I\"$JAVA_HOME/include/darwin\" -I\"$JAVA_HOME/include/linux/\" -shared -o target/reactivepi.so " + nativeDeviceSources.mkString(" ")
+
+  println(compileCommand)
+
+  compileCommand !
+}
---
+++ core/src/jni/nl_fizzylogic_reativepi_i2c_I2CDevice.c
@@ -0,0 +1,25 @@
+#import "nl_fizzylogic_reactivepi_i2c_I2CDevice.h"
+
+JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_initializeDevice(JNIEnv *, jobject, jstring) {
+    return -1;
+}
+
+JNIEXPORT void JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_closeDevice(JNIEnv *, jobject, jint) {
+    return -1;
+}
+
+JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_deviceWriteDirect(JNIEnv *, jobject, jint, jint, jbyteArray, jint, jint) {
+    return -1;
+}
+
+JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_deviceWrite(JNIEnv *, jobject, jint, jint, jint, jbyteArray, jint, jint) {
+    return -1;
+}
+
+JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_deviceReadDirect(JNIEnv *, jobject, jint, jint, jbyteArray, jint, jint) {
+    return -1;
+}
+
+JNIEXPORT jint JNICALL Java_nl_fizzylogic_reactivepi_i2c_I2CDevice_deviceRead(JNIEnv *, jobject, jint, jint, jint, jbyteArray, jint, jint) {
+    return -1;
+}
